911d71fe548a92a19a5a198a981327c4e4e29def
ud2 <sjx233@qq.com>
2023-10-24 12:13:55 +0800

    add `protocol` parameter

--- main.ts
+++ main.ts
@@ -12,21 +12,35 @@ const server = Deno.serve(async (req) => {
   if (url.pathname === "/favicon.ico") {
     return new Response(null, { status: 404 });
   }
+  const parseErrors: string[] = [];
+  let protocol: number | undefined;
+  parseProtocol: {
+    const protocolParam = url.searchParams.get("protocol");
+    if (protocolParam === null) {
+      break parseProtocol;
+    }
+    if (!/^-?\d+$/.test(protocolParam)) {
+      parseErrors.push("Protocol version must be an integer");
+      break parseProtocol;
+    }
+    protocol = Number(protocolParam);
+    if ((protocol | 0) !== protocol) {
+      parseErrors.push("Protocol version must fit in 32 bits");
+    }
+  }
   let timeout = defaultTimeout;
-  {
+  parseTimeout: {
     const timeoutParam = url.searchParams.get("timeout");
-    if (timeoutParam !== null) {
-      if (!/^\d+$/.test(timeoutParam)) {
-        return new Response("Timeout must be a non-negative integer", {
-          status: 400,
-        });
-      }
-      timeout = Number(timeoutParam);
-      if (timeout > maxTimeout) {
-        return new Response(`Timeout must be at most ${maxTimeout} ms`, {
-          status: 400,
-        });
-      }
+    if (timeoutParam === null) {
+      break parseTimeout;
+    }
+    if (!/^\d+$/.test(timeoutParam)) {
+      parseErrors.push("Timeout must be a non-negative integer");
+      break parseTimeout;
+    }
+    timeout = Number(timeoutParam);
+    if (timeout > maxTimeout) {
+      parseErrors.push(`Timeout must be at most ${maxTimeout} ms`);
     }
   }
   const parser = new URL("dummy://");
@@ -39,12 +53,16 @@ const server = Deno.serve(async (req) => {
     // handled below
   }
   if (!parser.hostname) {
-    return new Response("Invalid address", { status: 400 });
+    parseErrors.push("Invalid address");
+  }
+  if (parseErrors.length !== 0) {
+    return new Response(parseErrors.join("\n"), { status: 400 });
   }
   try {
     const json = await serverListPing({
       hostname: parser.hostname,
       port: parser.port ? parseInt(parser.port, 10) : undefined,
+      protocol,
       signal: AbortSignal.timeout(timeout),
     });
     return new Response(json, {
